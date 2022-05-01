dc4a82476c65be11f495c95cbe427508fb4035a8
midnight-madman <midnight-madman@proton.me>
2022-05-01 17:47:32 +0200

    🐧 Run script to fetch twitter data too

--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,13 @@
 from generate_url_rankings import generate_url_rankings
 from utils import upload_url_file_to_ipfs, upload_ranking_file_to_ipfs
 from get_metadata_for_urls import get_metadata_for_url_file
+from get_twitter_data import get_twitter_data
 
 
 def main():
-    # twitter_data_fname = get_twitter_data()
-    # get_metadata_for_url_file(twitter_data_fname)
-    # upload_url_file_to_ipfs(twitter_data_fname)
+    twitter_data_fname = get_twitter_data()
+    get_metadata_for_url_file(twitter_data_fname)
+    upload_url_file_to_ipfs(twitter_data_fname)
 
     url_rankings_fname = generate_url_rankings()
     upload_ranking_file_to_ipfs(url_rankings_fname)
