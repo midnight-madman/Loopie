f5957b96ed75e1489c2206641c725477ddf40d66
midnight-madman <midnight-madman@proton.me>
2022-05-01 17:29:52 +0200

    Test Github Action run without new tweets

--- scripts/main.py
+++ scripts/main.py
@@ -4,9 +4,9 @@ from get_metadata_for_urls import get_metadata_for_url_file
 
 
 def main():
-    twitter_data_fname = get_twitter_data()
-    get_metadata_for_url_file(twitter_data_fname)
-    upload_url_file_to_ipfs(twitter_data_fname)
+    # twitter_data_fname = get_twitter_data()
+    # get_metadata_for_url_file(twitter_data_fname)
+    # upload_url_file_to_ipfs(twitter_data_fname)
 
     url_rankings_fname = generate_url_rankings()
     upload_ranking_file_to_ipfs(url_rankings_fname)
