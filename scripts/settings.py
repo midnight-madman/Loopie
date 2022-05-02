758db8ca6b1730a447662a2885f55a4baf3868a8
midnight-madman <midnight-madman@proton.me>
2022-05-02 22:50:02 +0200

    [force ci]

--- scripts/settings.py
+++ scripts/settings.py
@@ -11,6 +11,6 @@ URL_FILES_IPFS_HASHES_FNAME = f'{DATA_DIR}url_files_ipfs_hashes.txt'
 WEEKLY_LEADERBOARD_IPFS_HASH_HISTORY_FNAME = f'{DATA_DIR}weekly_leaderboard_ipfs_hash_history.txt'
 
 USE_IPFS_TO_READ_DATA = True
-UPLOAD_DATA_TO_IPFS = False  # change to True before merging
+UPLOAD_DATA_TO_IPFS = False  # change to True before merging!
 
 from dev_settings import *
