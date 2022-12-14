e06d482d80026429f05b4a5fca91a140c770198d
midnight-madman <94986441+midnight-madman@users.noreply.github.com>
2022-12-14 11:40:17 +0100

    Add openai package and api key

--- scripts/settings.py
+++ scripts/settings.py
@@ -15,4 +15,6 @@ DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
 from dev_settings import *
