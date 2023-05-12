fbc0323d058c047ea5002cf2fa39fd562fe855f7
midnight-madman <94986441+midnight-madman@users.noreply.github.com>
2023-05-12 21:12:41 +0200

    remove dark theme styling

--- frontend/src/components/TagBubbleNavigator.tsx
+++ frontend/src/components/TagBubbleNavigator.tsx
@@ -21,14 +21,14 @@ function NavItem ({
         className={clsx(
           'relative block px-2 sm:px-3 py-1.5 sm:py-2 transition',
           isActive
-            ? 'text-slate-900 dark:text-slate-900 font-bold'
-            : 'text-slate-600 hover:text-slate-300 dark:hover:text-slate-300'
+            ? 'text-slate-900 font-bold'
+            : 'text-slate-600 hover:text-slate-300'
         )}
       >
         <span className="flex whitespace-nowrap">{children}</span>
         {isActive && (
           <span
-            className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-pink-500/0  via-pink-500/40 to-pink-500/0 dark:from-slate-400/0 dark:via-slate-400/40 dark:to-slate-400/0"/>
+            className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-pink-500/0  via-pink-500/40 to-pink-500/0"/>
         )}
       </Link>
     </li>
@@ -42,7 +42,7 @@ export const TagBubbleNavigator = () => {
 
   return <nav>
     <ul
-      className="flex rounded-full bg-white/90 px-1 sm:px-3 text-sm font-medium text-slate-800 shadow-lg shadow-slate-800/5 ring-1 ring-slate-900/5 backdrop-blur dark:bg-slate-800/90 dark:text-slate-200 dark:ring-white/10">
+      className="flex rounded-full bg-white/90 px-1 sm:px-3 text-sm font-medium text-slate-800 shadow-lg shadow-slate-800/5 ring-1 ring-slate-900/5 backdrop-blur">
       {map(take(values(NewsCategoriesEnum), NAV_ITEM_COUNT), (tag: NewsCategoriesEnum) =>
         <NavItem href={getHrefForTag(tag)} key={tag}>
           {tag} {getEmojiForCategory(tag)}
